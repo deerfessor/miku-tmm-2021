0f7ef2101322ff4071feeaa1407bc138feb36e06
deerfessor <37229049+deerfessor@users.noreply.github.com>
2021-10-31 18:49:08 +0900

    Update tmm_2021.py
    +параметр ИП

--- tmm_2021.py
+++ tmm_2021.py
@@ -1,20 +1,39 @@
 import random
-def d_car() :  #длина машины
+def d_car() :  #возвращает длину машины
     a=random.random()
-    if a<0.25 :
+    if a<0.20 :
         b=3.5
-    elif a<0.5 :
-        b=4
-    else :
+    elif a<0.4 :
         b=4.5
-    return (b+(random.random()%(0.5))+0.01)    
-area=float(input())
-useful_1=area//6.5
-useful_2=0
-car=d_car()
-while (area - 1.2) > car :
-    area=area - car - 1.2
-    useful_2+=1
+    else :
+        b=4
+   #print(b+(random.random()%(0.5))+0.01)
+    return (b+(random.random()%(0.5))+0.01)  
+
+def car_placer(open_space,count2) :  #возвращает колличество машин на участке
     car=d_car()
-print(useful_1)
-print(useful_2,area)
+    while (open_space - 1.2) > car :
+        c=random.random()
+        if c<0.7 :                       # машина встала в край зоны , 0.7 заменить на вероятность этого
+            open_space=open_space - car - 1.2
+            count2+=1
+            car=d_car()
+        else :                           # машина встала посреди зоны
+            d=random.random()      
+            open_space2=(open_space - 1.2 - car)*d  # вторая половина
+            open_space=open_space-open_space2       # первая половина
+            count2+=1
+            count2=(car_placer(open_space2,count2))  # количество машин на второй половине
+            car=d_car()
+    return count2
+area=float(input())
+repetition = int(input())    #число повторений
+useful_1=area//6.5  #машины при первом варианте
+useful_2=0          #машины при втором варианте
+useful_2=car_placer(area,useful_2)
+for t in range(repetition) :     
+    useful_2=car_placer(area,useful_2)
+    useful_1+=useful_1
+print(useful_1/useful_2) #если >1 то первый вариант эффективней
+print(useful_1)      #машины при первом варианте
+print(useful_2)      #машины при втором варианте
