08e689dd46f853d403c8ca510310850769adacfd
deerfessor <37229049+deerfessor@users.noreply.github.com>
2021-10-31 17:25:54 +0900

    Add files via upload

---
+++ tmm_2021.py
@@ -0,0 +1,19 @@
+import random
+def d_car() :  #длина машины
+    a=random.random()
+    if a<0.25 :
+        b=3.5
+    elif a<0.5 :
+        b=4
+    else :
+        b=4.5
+    return (b+(random.random()%(0.5))+0.01)    
+area=float(input())
+useful_1=area//6.5
+useful_2=0
+for i in range(15):
+    car=d_car()
+    area=area-car-1.2
+    useful_2+=1
+print(useful_1)
+print(useful_2,area)
